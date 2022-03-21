0b930521f83c32b319b67f25c960559dce988e15
LouisKwun <18797851097@163.com>
2022-03-22 00:37:25 +0800

    Initial commit

---
+++ cloud-provider-payment8002/src/main/java/com/kwun/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,23 @@
+package com.kwun.springcloud.service.impl;
+
+import com.kwun.springboot.entities.Payment;
+import com.kwun.springcloud.dao.PaymentDao;
+import com.kwun.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment){
+        return paymentDao.create(payment);
+    }
+
+    public Payment getPaymentById(Long id){
+        return paymentDao.getPaymentById(id);
+    }
+}
