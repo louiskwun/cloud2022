1bbd24702bf1b90a9ec3495e739d76bc8de1d93c
LouisKwun <18797851097@163.com>
2022-03-26 18:51:45 +0800

    3.26 Hystrix

---
+++ cloud-consumer-feign-order80/src/main/java/com/kwun/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,19 @@
+package com.kwun.springcloud.service;
+
+import com.kwun.springboot.entities.CommonResult;
+import com.kwun.springboot.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICER")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout();
+}
--- cloud-consumer-order80/src/main/java/com/kwun/springboot/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/kwun/springboot/OrderMain80.java
@@ -1,23 +1,20 @@
 package com.kwun.springboot;
 
+import com.kwun.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+//@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
-    }
-
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate(){
-        return new RestTemplate();
     }
 
 }
